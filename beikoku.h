885bb7f0fea9420dfc597ef3870b85864559bcac
Bartholomew Ponder <54320763+bartholomewponder@users.noreply.github.com>
2020-01-23 09:40:28 -0600

    Update beikoku.h

--- beikoku.h
+++ beikoku.h
@@ -9,11 +9,21 @@
 
 #include <SDL/SDL2.h.>
 
+//File type 
+typedef struct window {
+	SDL_Window* window,
+	
+} window;
+typedef struct graphic_file {
+	
+} graphic;
 typedef struct sound_file {
 	FILE* pFile;
 	SDL_AudioSpec audio;
 	const char* filename;
 	bool looping;
+	int time; 			//(in seconds) 
+	
 } sound;
 typedef struct stage_file {
 	FILE* pFile;
@@ -25,10 +35,17 @@ typedef struct shader_file {
 	const char* filename;
 	FILE* pFile;
 } shader;
+typedef struct char_type {
+	const char* name;
+	graphic char_sprite;
+	int health;
+	bullet bulltype;
+} character;
 
 extern bool initSDL();
 extern bool loadShaders(shader& gm_shader);
 extern bool loadSound(sound& gm_sound);
 extern bool loadStage(stage& gm_stage);
+extern bool loadImage(graphic& gm_image);
 
 #endif
